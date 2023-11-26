7fd42d5f4348bf34592976b52c562a7e0058dd59
Xuty <xutyxd@gmail.com>
2023-11-27 00:48:37 +0100

    feat(performance): Change app-config to try better performance

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,16 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 
-import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
-    provideAnimations(),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         // Register the ServiceWorker as soon as the application is stable
